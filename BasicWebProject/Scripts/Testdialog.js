952521f4efdab7936f17f0a9c317f5d5de470b97
Sjoerd Huisman <97072541@st.deltion.nl>
2020-03-17 10:29:52 +0100

    nieuwe css

---
+++ BasicWebProject/Scripts/Testdialog.js
@@ -0,0 +1,22 @@
+﻿var dialog = document.getElementById("shdialog")
+var footDialog = document.getElementById("ft_dialog")
+var headDialog = document.getElementById("hd_dialog_title")
+var contDialog = document.getElementById("cn_dialog")
+
+
+function fShowdmdialog(sWelke) {
+    dialog.style.top = '200px';
+    document.getElementById("overlay").style.display = "block";
+
+    if (sWelke == "inlog") {
+        headDialog.innerHTML = "Inloggen";
+        footDialog.innerHTML = "-Inlog-";
+        contDialog.innerHTML = document.getElementById("pieter").innerHTML;
+
+    }
+}
+
+function fClosedmdialog() {
+    document.getElementById('shdialog').style.top = '-300px';
+    overlay.style.display = "none";
+}
