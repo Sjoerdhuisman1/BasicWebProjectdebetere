b48ebbfd11f4404f97fe70e8adeb342559b7a87e
Dion03 <61456914+Dion03@users.noreply.github.com>
2020-04-24 10:24:53 +0200

    oeh

---
+++ BasicWebProject/App_Code/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft;
+
+namespace BasicWebProject.App_Code
+{
+    public class Song
+    {
+        public string id { get; set; }
+        public string artist { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public string time { get; set; }
+        public string file { get; set; }
+    }
+
+
+        public class Songs
+        {
+            public List<Song> songs { get; set; }
+
+        }
+    }
